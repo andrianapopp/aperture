21f4aa802c5f19b3e19fe6fd9808e79d43fa0f5a
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2022-11-02 10:04:45 +0200

    git init

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
+$( function() {
+    $( "#accordion" ).accordion();
+} );
